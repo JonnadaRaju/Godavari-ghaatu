c0dc3e8d273596241c91fbb467fd8fafa327e00e
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-13 15:22:53 +0530

    Fix required changes in database

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
-DATABASE_URL = "postgresql://raju:6014@localhost:5432/rajudb"
-
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, echo=settings.DEBUG,)
 
