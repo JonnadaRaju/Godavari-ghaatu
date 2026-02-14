6d18a8661235f98c7a9891986cfeb40189e73c6d
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-14 17:09:25 +0530

    Modified code to add variant id in Cart

--- backend/app/schemas/cart.py
+++ backend/app/schemas/cart.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel, Field
 from uuid import UUID
-from typing import List
+from typing import List, Optional
 
 
 class CartItemOut(BaseModel):
     id: UUID
     product_id: UUID
+    variant_id: Optional[UUID] = None,
+    variant_label: Optional[str] = None,
     quantity: int
     unit_price: float
     line_total: float
@@ -24,6 +26,7 @@ class CartOut(BaseModel):
         
 class AddCartItemIn(BaseModel):
     product_id: UUID
+    variant_id: Optional[UUID] = None
     quantity: int = Field(..., gt=0, description="Quantity must be greater than 0")
     
 class UpdateCartItemIn(BaseModel):
